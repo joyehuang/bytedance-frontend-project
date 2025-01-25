a937efae31713db32af811440d7b4b227728f87f
joye huang <huangdeshiou@gmail.com>
2025-01-25 12:00:06 +0800

    chore:add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
 });
